3b82efee253e50c46c023547adeb5a8dd48894f3
Annatar <jordyvw2@gmail.com>
2022-01-29 22:51:10 +0100

    - 2D Perlin noise - Simple Cavern generation

--- Assets/Resources/Jordy/Scripts/GameMaster.cs
+++ Assets/Resources/Jordy/Scripts/GameMaster.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : MonoBehaviour {
+public class GameMaster : MonoBehaviour 
+{
 
 	[SerializeField] private int spectatorMapWidth = 1800;
 	[SerializeField] private int spectatorMapHeight = 200;
@@ -12,7 +13,8 @@ public class GameMaster : MonoBehaviour {
 	public static long worldseed;
 
 	// Use this for initialization
-	void Awake () {
+	void Awake () 
+	{
 
 		mapHeight = spectatorMapHeight;
 		mapWidth = spectatorMapWidth;
@@ -21,13 +23,13 @@ public class GameMaster : MonoBehaviour {
 		blocks = new byte[mapWidth, mapHeight];
 
 		for (int x = 0; x < blocks.GetLength(0); x++)
-        {
-			int surfaceLayer = PerlinNoise.Noise(x, 1, mapWidth, worldseed);
-			Debug.Log(surfaceLayer + "SURFACE");
+		{
+			int surfaceLayer = PerlinNoise.Noise1D(x, 128, 8, mapWidth, worldseed);
+			//Debug.Log(surfaceLayer + "SURFACE");
 			int terrainHeight = surfaceLayer + 20;
 			print(terrainHeight);
 			for (int y = 0; y < blocks.GetLength(1); y++)
-            {
+			{
 				if (y == terrainHeight)
 				{
 					blocks[x, y] = 2; //grass
@@ -44,9 +46,27 @@ public class GameMaster : MonoBehaviour {
 				{
 					blocks[x, y] = 0; //air
 				}
-            }
-        }
-		
+
+				int dirtLump = PerlinNoise.Noise2D(x, y, 64, 60, mapWidth, worldseed);
+				//Debug.Log(cave + " cave");
+				if (dirtLump < 50 && blocks[x, y] == 1)
+				{
+					blocks[x, y] = 3;
+				}
+				int largeCavern = PerlinNoise.Noise2D(x, y, 64, 40, mapWidth, worldseed);
+				if (largeCavern < (50 - y/5) && blocks[x,y] != 0 && blocks[x,y] !=2)
+                {
+					blocks[x, y] = 0;
+                }
+				int smallCavern = PerlinNoise.Noise2D(x, y, 32, 80, mapWidth, worldseed);
+				if (smallCavern < (13) && blocks[x, y] != 0 && blocks[x, y] != 2)
+				{
+					blocks[x, y] = 0;
+				}
+
+			}
+
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Resources/Jordy/Scripts/PerlinNoise.cs
+++ Assets/Resources/Jordy/Scripts/PerlinNoise.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PerlinNoise{
+public class PerlinNoise
+{
 
 
-	public static int Noise (int x, int amplitude, int mapWidth, long seed){
+	public static int Noise1D(int x, int amplitude, int frequency, int mapWidth, long seed)
+	{
 
-		int frequency = 8; //This value represents the amount of noise sample points that are taken across the width of the map.
 		float noise = 0f;
-		amplitude = amplitude * (mapWidth / 10);
 		int octaves = (int)Mathf.Log((mapWidth / frequency), 2f);
-		
+
 		for (int currentOctave = 0; currentOctave < octaves; currentOctave++)
 		{
 			int sampleDistance = (mapWidth / frequency); //How big is chunksize used for assigning a noise value?
@@ -23,9 +23,9 @@ public class PerlinNoise{
 			int leftSample = RandomNum(currentOctave, sampleDistance, currentChunk, amplitude, seed);
 			int rightSample = RandomNum(currentOctave, sampleDistance, (currentChunk + 1), amplitude, seed);
 
-			Debug.Log("chunkSize is : " + sampleDistance);
-			Debug.Log("currentChunk is : " + currentChunk);
-			Debug.Log("chunkIndex is : " + chunkIndex);
+			//Debug.Log("chunkSize is : " + sampleDistance);
+			//Debug.Log("currentChunk is : " + currentChunk);
+			//Debug.Log("chunkIndex is : " + chunkIndex);
 			//Debug.Log("prog is : " + prog);
 			//Debug.Log("lS is : " + leftSample);
 			//Debug.Log("rS is : " + rightSample);
@@ -35,14 +35,56 @@ public class PerlinNoise{
 
 			frequency *= 2;
 			amplitude /= 2;
+
+		}
+		return (int)Mathf.Round(noise);
+
+	}
+	public static int Noise2D(int x, int y, int amplitude, int frequency, int mapWidth, long seed)
+	{
+		float noise = 0f;
+		int octaves = (int)Mathf.Log((mapWidth / frequency), 2f);
+		for (int currentOctave = 0; currentOctave < octaves; currentOctave++)
+		{
+			int sampleDistance = (mapWidth / frequency);
+			int offSet = Mathf.RoundToInt(sampleDistance / 3);
+			int currentChunkX = Mathf.FloorToInt((x + offSet)/ sampleDistance);
+			int currentChunkY = Mathf.FloorToInt((y +offSet)/ sampleDistance);
+			int chunkIndexX = (x + offSet) % sampleDistance;
+			int chunkIndexY = (y + offSet) % sampleDistance;
+			float progX = (float)chunkIndexX / sampleDistance;
+			float progY = (float)chunkIndexY / sampleDistance;
+
+			int bottomLeftSample = RandomNum2D	(currentOctave, sampleDistance, currentChunkX,		currentChunkY,		amplitude, seed);
+			int bottomRightSample = RandomNum2D	(currentOctave, sampleDistance, currentChunkX + 1,	currentChunkY,		amplitude, seed);
+			int topLeftSample = RandomNum2D		(currentOctave, sampleDistance, currentChunkX,		currentChunkY + 1,	amplitude, seed);
+			int topRightSample = RandomNum2D	(currentOctave, sampleDistance, currentChunkX + 1,	currentChunkY + 1,	amplitude, seed);
+
+			float bottomEdge = (bottomLeftSample + (bottomRightSample - bottomLeftSample) * progX);
+			float topEdge = (topLeftSample + (topRightSample - topLeftSample) * progX);
+
+			noise += (bottomEdge + (topEdge - bottomEdge) * progY);
+
+			frequency *= 2;
+			if (amplitude > 1)
+            {
+				amplitude /= 2;
+			}
 			
 		}
-		return (int)Mathf.Round (noise);
-		
+
+		return (int)Mathf.Round(noise);
 	}
-	public static int RandomNum (int octave, int size, int currentChunk, int scale, long seed){
-		int value = Mathf.Abs((int)(seed + (octave + size + 13)^(currentChunk+1) * 60943) % scale);
-		Debug.Log(value + "RandomNum");
+	public static int RandomNum(int octave, int size, int currentChunk, int scale, long seed)
+	{
+		int value = Mathf.Abs((int)(seed + (octave + size + 13) ^ (currentChunk + 5) * 60943) % scale);
+		//Debug.Log(value + "RandomNum");
+		return value;
+	}
+	public static int RandomNum2D(int octave, int size, int currentChunkX, int currentChunkY, int scale, long seed)
+	{
+		int value = Mathf.Abs((int)(seed + (octave + 300)^3 * (size + 500)^3 + ((currentChunkX + 201) * (currentChunkY + 201)) * 60943) % scale);
+		//Debug.Log(value + "RandomNum");
 		return value;
 	}
 }
